43d3e56a42e7ee693ac60db1d5f8cc46902604bb
NAMTVPH23205 <127822190+NAMTVPH23205@users.noreply.github.com>
2023-10-04 21:46:13 +0700

    Xong CRUD KhachHang

--- springboot-backend/src/main/java/com/poly/springboot/controller/AddressController.java
+++ springboot-backend/src/main/java/com/poly/springboot/controller/AddressController.java
@@ -1,30 +1,45 @@
 package com.poly.springboot.controller;
 
 import com.poly.springboot.dto.requestDto.AddressRequestDto;
+import com.poly.springboot.entity.Address;
 import com.poly.springboot.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/address")
+@RequestMapping("/api/")
 public class AddressController {
     @Autowired
     private AddressService addressService;
 
-    @GetMapping("/hien-thi")
+    @GetMapping("address")
     public ResponseEntity<?> hienThi() {
         return ResponseEntity.ok(addressService.getAll());
     }
 
-    @PostMapping("/add")
+    @PostMapping("create-address")
     public ResponseEntity<?> add(AddressRequestDto addressRequestDto) {
         return ResponseEntity.ok(addressService.add(addressRequestDto));
     }
-    
 
+    @PutMapping("update-address/{id}")
+    public ResponseEntity<?> update(AddressRequestDto addressRequestDto, @PathVariable Long id){
+        Address address = addressService.update(addressRequestDto,id);
+        return ResponseEntity.ok(address);
+    }
+
+    @DeleteMapping("delete-address")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        String message = addressService.delete(id);
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("address/{id}")
+    public ResponseEntity<Address> getAddress(@PathVariable Long id){
+        Address address = addressService.findAddressById(id);
+        return ResponseEntity.ok(address);
+    }
 
 }

e1b04d33f96208d69fb5493e4216f354d573abf2
Le Dang Thanh <ledangthanh22062002@gmail.com>
2023-10-10 09:00:50 +0700

    update

--- springboot-backend/src/main/java/com/poly/springboot/dto/responseDto/AddressResponseDto.java
+++ springboot-backend/src/main/java/com/poly/springboot/dto/responseDto/AddressResponseDto.java
@@ -5,13 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddressResponeDto {
+public class AddressResponseDto {
 
     private  Long id;
 
--- springboot-backend/src/main/java/com/poly/springboot/service/AddressService.java
+++ springboot-backend/src/main/java/com/poly/springboot/service/AddressService.java
@@ -1,15 +1,14 @@
 package com.poly.springboot.service;
 
 import com.poly.springboot.dto.requestDto.AddressRequestDto;
-import com.poly.springboot.dto.responseDto.AddressResponeDto;
+import com.poly.springboot.dto.responseDto.AddressResponseDto;
 import com.poly.springboot.entity.Address;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 public interface AddressService {
-    List<AddressResponeDto> getAll();
+    List<AddressResponseDto> getAll();
 
     Address add(AddressRequestDto addressRequestDto);
 
